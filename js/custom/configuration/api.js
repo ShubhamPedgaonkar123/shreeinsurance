75d3cd3db2b45db2c9e3814f07c9d0f366dbfc38
shuhamPedgaonkar <spedgaonkar@pcpl.io>
2022-09-02 14:58:15 +0530

    11 Commit

--- js/custom/configuration/api.js
+++ js/custom/configuration/api.js
@@ -23,7 +23,6 @@ async function login(params) {
     fetch(url, {
             method: 'POST', // or 'PUT'
             mode: 'cors',
-            // credentials: 'same-origin',
             headers: {
                 'Content-Type': 'application/json',
             },
